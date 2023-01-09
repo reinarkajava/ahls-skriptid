94c818e3bffbf2795dda7954f29974489eebbdc0
Reinar Kajava <reinar.kajava@voco.ee>
2023-01-09 13:34:19 +0000

    kasutajanime küsimise asemel tuleb see nüüd lisada parameetrina

--- kasutaja.sh
+++ kasutaja.sh
@@ -6,14 +6,13 @@
 
 
 #kasutaja sisestab nime
-echo "Sisesta Kasutajanimi: "
-read Kasutajanimi
 
 #Paar teksti
 echo "Luuakse kasutaja nimega $Kasutajanimi koos kataloogiga"
 echo "Kasutaja tegemise ajal küsitakse salasõna, aga see eemaldatakse pärast kasutaja loomist automaatselt."
 #Kasutaja luuakse, küsitakse muid parameeterid nagu password jne.
 sleep 5
-adduser $Kasutajanimi
+#$1 Näitab missugune kasutajanimi antakse loodavale kasutajale. $1 on esimene shelliskriptile edastatud käsurea argument $2 on teine ja nii edasi.
+adduser $1
 #Eelnevalt lisatud password eemaldatakse
-passwd -d $Kasutajanimi
+passwd -d $1
