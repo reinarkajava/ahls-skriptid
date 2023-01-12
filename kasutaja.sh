5e5feefe052c7862a7999b0b56a6f22696fdd245
Reinar Kajava <reinar.kajava@voco.ee>
2023-01-12 09:09:21 +0000

    uuendus

--- kasutaja.sh
+++ kasutaja.sh
@@ -11,8 +11,8 @@
 echo "Luuakse kasutaja nimega $Kasutajanimi koos kataloogiga"
 echo "Kasutaja tegemise ajal küsitakse salasõna, aga see eemaldatakse pärast kasutaja loomist automaatselt."
 #Kasutaja luuakse, küsitakse muid parameeterid nagu password jne.
-sleep 5
+sleep 2
 #$1 Näitab missugune kasutajanimi antakse loodavale kasutajale. $1 on esimene shelliskriptile edastatud käsurea argument $2 on teine ja nii edasi.
-adduser $1
+useradd $1 -m -s /bin/bash
 #Eelnevalt lisatud password eemaldatakse
 passwd -d $1
